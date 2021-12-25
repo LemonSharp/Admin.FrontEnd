bb6e72ffc920f51182cfe21f9ded3b436fe337be
Richeir <315310840@qq.com>
2021-12-26 00:11:13 +0800

    [add] project init

---
+++ BootstrapBlazorApp.Shared/Pages/VaccinationPlace.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazorApp.Shared.Pages;
+
+public class ItemModel
+{
+    [NotNull]
+    public string Name { get; set; }
+
+    [NotNull]
+    public string Address { get; set; }
+
+    [NotNull]
+    public DateTime? DateTime { get; set; }
+}
+
+public partial class VaccinationPlace
+{
+    [NotNull]
+    private List<ItemModel>? Items { get; set; }
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Items = FillItems();
+    }
+
+    private List<ItemModel> FillItems()
+    {
+        var items = new List<ItemModel>();
+        items.Add(new ItemModel()
+        {
+            DateTime = DateTime.Now,
+            Name = "1",
+            Address = "2"
+        });
+
+        return items;
+    }
+}
